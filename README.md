a090ee348636f2db1b7a4daf2f9db42b7372c2c5
andyyounes <99356388+andyyounes@users.noreply.github.com>
2025-06-30 22:35:23 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Crazyflie-leader-follower
+# crazyflies-leader-follower
+Simple leader go to waypoints - and follower subscirbes to it's position
+
+```bash
+source install/setup.bash
+ros2 launch crazyflie launch.py rviz:=True backend:=sim
+ros2 run crazyflie_leader_follower run --ros-args -p use_sim_time:=True
+```
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+import os
+from glob import glob
+
+package_name = 'crazyflie_leader_follower'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        # Install package.xml
+        (os.path.join('share', package_name), ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Andy',
+    maintainer_email='andy.younes@csie.am',
+    description='Crazyflie leader-follower example',
+    license='TODO',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'run = crazyflie_leader_follower.run:main',
+        ],
+    },
+)
+
